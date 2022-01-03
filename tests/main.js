2d3a6d7347b1bde525697c5a5aa33e8ea62479ed
Ankit Raj <ankit.raj928@gmail.com>
2022-01-03 15:54:09 +0530

    update

--- tests/main.js
+++ tests/main.js
@@ -1,7 +1,14 @@
 import { sleep } from "k6";
-import stress from "./stress.test.js";
-
-export default function () {
+import stress from "./main.test.js";
+import index from "./index.test.js";
+import tls from "./tls_skip_cert.test.js";
+import tls_version from "./tls_version.test.js";
+export default () => {
   stress();
   sleep(1);
+  index();
+  sleep(1);
+  tls();
+  sleep(1);
+  tls_version();
 }
